fc2878cc546b2ad5c1c13b2ae48a146fd8053a0b
Guilherme Guelere <guilhermeg.dev@gmail.com>
2025-04-25 21:49:03 -0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type React from "react"
+import "./globals.css"
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Guilherme Guelere",
+  description: "Portfólio Pessoal",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
